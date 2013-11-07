4bc77b8cb411b9635455f065f2cdfb0d86180ad2
homonecloco <ricardo.ramirez-gonzalez@tgac.ac.uk>
2013-11-07 10:41:27 +0000

    Added tool to get the best contig with blat

---
+++ bin/find_best_blat_hit.rb
@@ -0,0 +1,32 @@
+#!/usr/bin/env ruby
+require 'bio'
+
+def load_blat_alignments (blat_filename, best_aln)
+  blat_aln = Bio::Blat::Report.new(Bio::FlatFile.open(blat_filename).to_io)
+  blat_aln.each_hit() do |hit|
+    current_matches = hit.match 
+    current_name = hit.query_id
+    current_identity = hit.percent_identity
+    current_score = hit.score
+    #p current_name
+
+    best = best_aln[current_name]
+
+    if best == nil 
+      best_aln[current_name] = hit
+    else
+      if current_score > best.score
+        best_aln[current_name] = hit
+      end
+    end
+  end
+end
+
+blat_file=ARGV[0]
+best_aln = Hash.new
+
+load_blat_alignments( blat_file,best_aln)
+puts "QUERY\tTARGET"
+best_aln.each do |k, hit|
+  puts "#{k}\t#{hit.target_id}"
+end
