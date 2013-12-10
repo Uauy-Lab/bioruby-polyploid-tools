a0b84cbe192d4c46e4cac6222a0fe70cb6e212b4
Ricardo Ramirez-Gonzalez <ramirezr@n57141.tgaccluster>
2013-12-10 15:29:12 +0000

    Fixed order in the table

--- bin/snps_between_bams.rb
+++ bin/snps_between_bams.rb
@@ -27,7 +27,7 @@ block_size=300
 main_table="#{output_prefix}_#{block_size}_table.csv"
 
 table_file = File.open(main_table, "w")
-table_file.puts "gene\tlength\tsnps_1\tcalled_1\tsnps_per_1k_1\tsnps_2\tsnps_per_1k_2}\tcalled_2\tsnps_tot\tsnps_per_1k_tot"
+table_file.puts "gene\tlength\tsnps_1\tcalled_1\tsnps_per_#{block_size}_1\tsnps_2\tcalled_2\tsnps_per_#{block_size}_2\tsnps_tot\tsnps_per_1k_tot"
 
 hist_1= Hash.new(0)
 hist_2= Hash.new(0)
@@ -37,35 +37,35 @@ fasta_db.index.entries.each do | r |
   #Np r.get_full_region
   #container.process_region( { :region => r.get_full_region.to_s, :output_file => output_file } )
   region=r.get_full_region
-  
-  
+
+
   begin
 
-  cons_1 = bam1.consensus_with_ambiguities({:region=>region, :case=>true})
-  cons_2 = bam2.consensus_with_ambiguities({:region=>region, :case=>true})
-  if cons_1 != cons_2
-    
-    snps_1 = cons_1.count_ambiguities
-    snps_2 = cons_2.count_ambiguities
-    
-    called_1 = cons_1.upper_case_count
-    called_2 = cons_2.upper_case_count
-    
-    snps_tot = Bio::Sequence.snps_between(cons_1, cons_2)
-  
-    snps_per_1k_1   = (block_size * snps_1.to_f   ) / called_1
-    snps_per_1k_2   = (block_size * snps_2.to_f   ) / called_2
-    snps_per_1k_tot = (block_size * snps_tot.to_f ) / region.size
-    
-    hist_1[snps_per_1k_1.to_i] += 1
-    hist_2[snps_per_1k_2.to_i] += 1
-  
-    table_file.puts "#{r.id}\t#{region.size}\t#{snps_1}\t#{called_1}#{snps_per_1k_1}\t#{snps_2}\t#{snps_per_1k_2}\t#{called_2}\t#{snps_tot}\t#{snps_per_1k_tot}"
-    fasta_file.puts ">#{r.id}_1"
-    fasta_file.puts "#{cons_1}"
-    fasta_file.puts ">#{r.id}_2"
-    fasta_file.puts "#{cons_2}"
-  end
+    cons_1 = bam1.consensus_with_ambiguities({:region=>region, :case=>true})
+    cons_2 = bam2.consensus_with_ambiguities({:region=>region, :case=>true})
+    if cons_1 != cons_2
+
+      snps_1 = cons_1.count_ambiguities
+      snps_2 = cons_2.count_ambiguities
+
+      called_1 = cons_1.upper_case_count
+      called_2 = cons_2.upper_case_count
+
+      snps_tot = Bio::Sequence.snps_between(cons_1, cons_2)
+
+      snps_per_1k_1   = (block_size * snps_1.to_f   ) / called_1
+      snps_per_1k_2   = (block_size * snps_2.to_f   ) / called_2
+      snps_per_1k_tot = (block_size * snps_tot.to_f ) / region.size
+
+      hist_1[snps_per_1k_1.to_i] += 1
+      hist_2[snps_per_1k_2.to_i] += 1
+
+      table_file.puts "#{r.id}\t#{region.size}\t#{snps_1}\t#{called_1}#{snps_per_1k_1}\t#{snps_2}\t#{called_2}\t#{snps_per_1k_2}\t#{snps_tot}\t#{snps_per_1k_tot}"
+      fasta_file.puts ">#{r.id}_1"
+      fasta_file.puts "#{cons_1}"
+      fasta_file.puts ">#{r.id}_2"
+      fasta_file.puts "#{cons_2}"
+    end
   rescue Exception => e
     $stderr.puts "Unable to process #{region}: #{e.to_s}"
   end
