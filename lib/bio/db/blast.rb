77c49a4f06460b98ffd725631d8ed78c464adf3e
homonecloco <ricardo.ramirez-gonzalez@tgac.ac.uk>
2018-01-12 12:02:15 +0000

    Addeed sugar and initial run supporting blast

--- lib/bio/db/blast.rb
+++ lib/bio/db/blast.rb
@@ -3,7 +3,6 @@ module Bio::DB::Blast
 	def self.to_sugar(line)
 		fields = line.split("\t")[0..8]
 		
-
 		if  fields[3] =="-1"
 			fields[3] = "-"
 			fields[2] = fields[2].to_i - 1
@@ -11,32 +10,85 @@ module Bio::DB::Blast
 			fields[3] = "+"
 			fields[1] = fields[1].to_i - 1
 		end
-
-		if  fields[7] =="-1"
+		if fields[7] =="-1"
 			fields[7] = "-"
 			fields[6] = fields[6].to_i - 1
 		else
 			fields[7] = "+"
 			fields[5] = fields[5].to_i - 1
 		end
-
 		fields.join(" ")
+	end
+
+	def self.to_vulgar(line)
+		qseq, sseq = line.split("\t")[12..13]
+
+		len = qseq.length
+		l_status = ""
+		l_len = 0
+		str = Array.new
+		statuses = ""
+		for i in 0..len
+			if qseq[i] == "-"
+				status = "D"
+			elsif sseq[i] == "-"
+				status = "I"
+			else
+				status = "M"
+			end
+			statuses << status
+		end
+		statuses.split('').each do |e| 
+			if l_status != e
+				case l_status
+				when "M"
+					str << ["M", l_len, l_len]
+				when "I"
+					str << ["G", l_len, 0]
+				when "D"
+					str << ["G", 0, l_len]
+				end
+				l_len = 0
+			end
+			l_status = e
+			l_len += 1
+		end
+		l_len -= 1
+		case l_status
+		when "M"
+			str << ["M", l_len, l_len]
+		when "I"
+			str << ["G", l_len, 0]
+		when "D"
+			str << ["G", 0, l_len]
+		end
+
+		str.flatten!.join(" ")
+	end
+
+	def self.to_exo(line)
+		arr = Array.new
+		arr << "RESULT:"
+		arr << to_sugar(line)
+		arr << line.split("\t")[9..11]
+		arr << "."
+		arr << to_vulgar(line)
+		arr.join("\t")
 	end
 
 	def self.align(opts={})
 		target=opts[:target]
 		query=opts[:query]
-		cmdline = "blastn -query #{query} -db #{target} -outfmt '6 qseqid qstart qend qframe sseqid sstart send sframe score pident	qlen	slen	qseq sseq'"
+		cmdline = "blastn -query #{query} -db #{target} -outfmt '6 qseqid qstart qend qframe sseqid sstart send sframe score pident qlen slen qseq sseq'"
 
 		status, stdout, stderr = systemu cmdline
 		if status.exitstatus == 0
 			alns = Array.new unless block_given?
-			stdout.each_line do |line|
-				puts line
+			stdout.each_line do |e_l|
+				#puts e_l
+				line = to_exo(e_l)
+				#puts line
 				arr = line.split("\t")
-
-
-
 				aln = Bio::DB::Exonerate::Alignment.parse_custom(line) 
 				if aln
 					if block_given?
--- test/test_blast.rb
+++ test/test_blast.rb
@@ -20,15 +20,26 @@ class TestBlast < Test::Unit::TestCase
   def test_blast_to_exo
     lines = File.readlines(Blast_file)
     expected = [
-      "BS00068396_51 0 101 + 2AS_5222932 3015 2914 - 99",
+    "BS00068396_51 0 101 + 2AS_5222932 3015 2914 - 99",
     "BS00068396_51 0 101 + 2DS_5334799 6812 6913 + 99",
     "BS00068396_51 0 101 + 2BS_5245544 4549 4651 + 87",
     "BS00068396_51 101 0 - 2BS_5163353 7425 7323 - 87"]
 
+    expected_v = [
+      "M 101 101", 
+      "M 101 101",
+      "M 69 69 G 0 1 M 32 32",
+      "M 69 69 G 1 0 M 32 32"]
+
     lines.each_with_index do |line , i|
       tmp =  Bio::DB::Blast.to_sugar(line)
-      assert_equal(tmp, expected[i], "Error in line #{i} of the cigar")
-     
+      assert_equal(tmp, expected[i], "Error in line #{i} of the SUGAR")
+      tmp =   Bio::DB::Blast.to_vulgar(line)
+      assert_equal(tmp, expected_v[i], "Error in line #{i} of the Vulgar")
+
+      tmp =   Bio::DB::Blast.to_exo(line)
+      puts tmp
+    
     end
     
   end
