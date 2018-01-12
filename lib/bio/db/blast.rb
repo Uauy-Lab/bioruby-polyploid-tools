6bf77f8fed5bb3c4f1c25236883413f955a66f47
homonecloco <ricardo.ramirez-gonzalez@tgac.ac.uk>
2018-01-12 10:56:04 +0000

    Test to convert sugar from blast fmt 6

--- lib/bio/db/blast.rb
+++ lib/bio/db/blast.rb
@@ -1,5 +1,28 @@
 module Bio::DB::Blast
 
+	def self.to_sugar(line)
+		fields = line.split("\t")[0..8]
+		
+
+		if  fields[3] =="-1"
+			fields[3] = "-"
+			fields[2] = fields[2].to_i - 1
+		else
+			fields[3] = "+"
+			fields[1] = fields[1].to_i - 1
+		end
+
+		if  fields[7] =="-1"
+			fields[7] = "-"
+			fields[6] = fields[6].to_i - 1
+		else
+			fields[7] = "+"
+			fields[5] = fields[5].to_i - 1
+		end
+
+		fields.join(" ")
+	end
+
 	def self.align(opts={})
 		target=opts[:target]
 		query=opts[:query]
@@ -10,7 +33,11 @@ module Bio::DB::Blast
 			alns = Array.new unless block_given?
 			stdout.each_line do |line|
 				puts line
-				aln = Bio::DB::ExonerateAlignment.parse_custom(line) 
+				arr = line.split("\t")
+
+
+
+				aln = Bio::DB::Exonerate::Alignment.parse_custom(line) 
 				if aln
 					if block_given?
 						yield aln
