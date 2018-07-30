bbe18a927dcb452bb0f8ca29d917fecf6ea29243
Ricardo H. Ramirez-Gonzalez <ricardo.ramirez-gonzalez@jic.ac.uk>
2018-07-30 13:21:12 +0100

    Added stub of the vcfToPolyMarker files

---
+++ bin/vcfToPolyMarker.rb
@@ -0,0 +1,8 @@
+#/usr/bin/env ruby
+require 'optparse'
+
+require 'csv'
+require 'bio'
+require 'bio-samtools'
+
+
---
+++ lib/bio/PolyploidTools/SNPFromVCF.rb
@@ -0,0 +1,9 @@
+
+require_relative "SNPSequence"
+require 'bio-samtools'
+module Bio::PolyploidTools
+  class SNPMutant < SNPSequence
+
+  end
+end
+
